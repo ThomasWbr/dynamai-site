36fd0a844ac1c5ce97736532a2ae82bbd4f02a23
ThomasWbr <tfac28@gmail.com>
2026-03-17 22:03:39 -0300

    mensagem

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,15 +4,18 @@ const string1 = "O Diária Express é um sistema proposto para centralizar e org
 const string2 = "O Actae é um aplicativo projetado para registrar acontecimentos de forma cronológica, precisa e organizada, transformando anotações feitas no momento dos fatos em relatórios formais. A ferramenta permite que o usuário registre eventos diretamente pelo celular — adicionando descrições, datas, horários, fotos e outras evidências — criando automaticamente uma linha do tempo estruturada. Ao final, o sistema pode gerar um documento profissional em formato de relatório ou ata, útil para fins técnicos, administrativos ou jurídicos, garantindo fidelidade dos acontecimentos e evitando a perda de informações que normalmente ocorreria ao tentar reconstruir os fatos posteriormente."
 const string3 = "O Tablinum é um aplicativo completo para a administração de condomínios, desenvolvido para tornar a gestão mais simples, eficiente e transparente. Com uma interface intuitiva, a plataforma centraliza o controle financeiro, a comunicação entre síndicos e moradores, a gestão de documentos e o acompanhamento de ocorrências em um só lugar. O Tablinum permite automatizar tarefas, reduzir a burocracia e melhorar a organização do condomínio, proporcionando mais praticidade no dia a dia e uma experiência mais moderna para todos os usuários."
 
+const cor1 = "#6632FE"
+const cor2 = "#E34232"
+
 const cards = [
-    { id: 1, titulo: "Diária Express", descricao: string1},
-    { id: 2, titulo: "Actae", descricao: string2},
-    { id: 3, titulo: "Tablinum", descricao: string3}
+    { id: 1, titulo: "Diária Express", descricao: string1, cor: cor1},
+    { id: 2, titulo: "Actae", descricao: string2, cor: cor1},
+    { id: 3, titulo: "Tablinum", descricao: string3, cor: cor2}
 ]
 
-function CreateCard({ titulo, descricao}) {
+function CreateCard({titulo, descricao, cor}) {
     return (
-        <div className="dyCard">
+        <div className="dyCard" style={{border: `2.5px solid ${cor}`, color: cor}}>
             <h3 className="dyCardT">{titulo}</h3>
             <p className="dyCardP">{descricao}</p>
         </div>
@@ -23,7 +26,7 @@ function Card() {
     return (
         <div className="cardContainer">
             {cards.map(
-                (c) => (<CreateCard key={c.id} titulo={c.titulo} descricao={c.descricao} />)
+                (c) => (<CreateCard key={c.id} titulo={c.titulo} descricao={c.descricao} cor={c.cor}/>)
             )}
         </div>
     )
